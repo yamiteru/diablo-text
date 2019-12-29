c5093854d65bf9f1eca86d86a17cfed0ff01adc9
Yamiteru <office@yamiteru.xyz>
2019-12-29 00:33:08 +0000

    Poznámky k itemu

--- src/lib/item/item.js
+++ src/lib/item/item.js
@@ -4,17 +4,25 @@ import data from "./data.js";
 class Item extends Class {
     constructor(obj = {}) {
         super();
-        // this.neco = obj.neco;
-
-        console.log(obj);
 
         this.name = obj.name;
         this.lvl = obj.lvl || 1;
-    
+
+        // - vlastník/držitel itemu??
+        // - opotřebovanost itemu
+        // - magický efekty
+        // - sockety na kameny
+        // - attack speed (rychlost)
+        // - range (vzdálenost)
+        // - hands (one-handed, two-handed)
+        // - slot (levá ruka, hlava, rukavice, ..)
+
+        // inspirace D2: https://diablo.gamepedia.com/Items_(Diablo_II)
+
         this.attrs = {
-            strength: obj.strength || 5,
-            vitality: obj.vitality || 5,
-            damage: obj.damage ||  2
+            strength: obj.strength || 5,    // budeme na základě classy postavy rozlišovat na čem se staví dmg (např. barbar síla, kouzelník inteligence, atp..)?
+            vitality: obj.vitality || 5,    // na základě vitality by se měl vypočítávat život
+            damage: obj.damage ||  2    // dmg neni atribut a měl by být vypočítavanej a ne natvrdo
         }
     }
 }
